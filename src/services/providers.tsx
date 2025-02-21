77fc1e82406d749fc0f00990547bb4f7e97f714d
Kumar Abhinav <97421185+0xabhinav@users.noreply.github.com>
2025-02-21 23:58:36 +0530

    Disable confirmation modal

--- src/services/providers.tsx
+++ src/services/providers.tsx
@@ -24,7 +24,8 @@ export default function Providers({children}: {children: React.ReactNode}) {
             solana: { 
               createOnLogin: 'users-without-wallets', 
             }, 
-          }, 
+            showWalletUIs: false,
+          }
         }}
       >
         {children}
