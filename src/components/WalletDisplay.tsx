cd7d438099ebb09f27546e65481e8eeeb62d8ffb
Bharath Singh <bharathsingh11.bs@gmail.com>
2025-03-10 09:22:20 +0530

    remove solana wallet address copy

--- src/components/WalletDisplay.tsx
+++ src/components/WalletDisplay.tsx
@@ -10,19 +10,6 @@ interface WalletDisplayProps {
 export function WalletDisplay({ evmWallets, solanaWallets }: WalletDisplayProps) {
   return (
     <div className="flex gap-3 p-5 justify-end border-b border-gray-200 bg-white">
-      {solanaWallets.map((wallet) => (
-        <div key={wallet.address} className="flex items-center">
-          <CopyButton
-            text={wallet.address || ''}
-            displayText={wallet.address ? 
-              `SOL: ${wallet.address.slice(0, 6)}...${wallet.address.slice(-4)}` : 
-              ''
-            }
-            icon={<WalletIcon />}
-            title="Click to copy Solana wallet address"
-          />
-        </div>
-      ))}
       {evmWallets.map((wallet) => (
         <div key={wallet.address} className="flex items-center">
           <CopyButton
